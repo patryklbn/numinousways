20c8e81911ed7518931165900c7c5c27bd13d51d
patrykgronczewski <40599647@live.napier.ac.uk>
2024-12-13 15:40:29 +0000

    Add orderBy to getFacilitators query in firestore_service

--- lib/models/facilitator.dart
+++ lib/models/facilitator.dart
@@ -1,35 +1,69 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+class SocialMediaLink {
+  final String platform;
+  final String url;
+
+  SocialMediaLink({required this.platform, required this.url});
+
+  factory SocialMediaLink.fromMap(Map<String, dynamic> data) {
+    return SocialMediaLink(
+      platform: data['platform'] ?? '',
+      url: data['url'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'platform': platform,
+      'url': url,
+    };
+  }
+}
+
 class Facilitator {
   final String id;
   final String name;
   final String role;
   final String photoUrl;
+  final String? description;
+  final List<SocialMediaLink>? socialMediaLinks;
 
   Facilitator({
     required this.id,
     required this.name,
     required this.role,
     required this.photoUrl,
+    this.description,
+    this.socialMediaLinks,
   });
 
-  // Factory constructor to create Facilitator from Firestore DocumentSnapshot
   factory Facilitator.fromDocument(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
+    List<SocialMediaLink>? socialLinks;
+    if (data['socialMediaLinks'] != null) {
+      socialLinks = (data['socialMediaLinks'] as List)
+          .map((e) => SocialMediaLink.fromMap(e as Map<String, dynamic>))
+          .toList();
+    }
     return Facilitator(
       id: doc.id,
       name: data['name'] ?? '',
       role: data['role'] ?? '',
       photoUrl: data['photoUrl'] ?? '',
+      description: data['description'],
+      socialMediaLinks: socialLinks,
     );
   }
 
-  // Method to convert Facilitator to Map for Firestore
   Map<String, dynamic> toMap() {
     return {
       'name': name,
       'role': role,
       'photoUrl': photoUrl,
+      if (description != null) 'description': description,
+      if (socialMediaLinks != null)
+        'socialMediaLinks': socialMediaLinks!.map((e) => e.toMap()).toList(),
     };
   }
 }
